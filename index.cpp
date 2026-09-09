8967db540c4e923e53ec28b882acc7bf4e32014e
Prince Yadav <py0503156@gmail.com>
2026-09-09 22:05:24 +0530

    done brother

--- index.cpp
+++ index.cpp
@@ -2,5 +2,6 @@
 using namespace std;
 int main(){
 cout<<"hello there!";
+cout<<"\n let's start the journey for git/github";
 return 0;
 }
--- new3.py
+++ new3.py
@@ -1,1 +1,2 @@
 print("Hello uncle")
+print("This is wow")
